0a9abd44d74814bfe319512e674dc76b975f22be
mochapoke <71932072+mochapoke@users.noreply.github.com>
2020-12-14 21:05:42 +0900

    add: header, footer, css

--- app.js
+++ app.js
@@ -16,14 +16,10 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 
-
-
-
-
-
-
-
-
+app.get('/', (req,res) => {
+  res.render('home', 
+    {homeContent: homeStartingContent} );
+})
 
 
 
