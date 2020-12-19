a4c0c30b729eace3d09f8aa9c729a23c6888b058
mocha <syda07@gmail.com>
2020-12-19 18:23:47 +0900

    추가: mongoose연결, compose에서 추가하면 mongoDB에서 확인가능

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require('lodash');
+const mongoose = require('mongoose');
+
+mongoose.set('useUnifiedTopology', true );
+mongoose.set('useNewUrlParser', true );
+
+mongoose.connect('mongodb://localhost:27017/blogDB');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -10,7 +16,20 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 const app = express();
 
 
-let posts = [];
+const postSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, 'please enter the title']
+  },
+  content: String
+})
+
+const Post = mongoose.model('Post', postSchema);
+
+const samplePost = new Post({
+  title: 'Sample',
+  content: '국가의 세입·세출의 결산, 국가 및 법률이 정한 단체의 회계검사와 행정기관 및 공무원의 직무에 관한 감찰을 하기 위하여 대통령 소속하에 감사원을 둔다. 국회의원은 법률이 정하는 직을 겸할 수 없다. 국회는 법률에 저촉되지 아니하는 범위안에서 의사와 내부규율에 관한 규칙을 제정할 수 있다. 국가는 법률이 정하는 바에 의하여 재외국민을 보호할 의무를 진다. 대한민국의 국민이 되는 요건은 법률로 정한다. 대통령은 법률이 정하는 바에 의하여 사면·감형 또는 복권을 명할 수 있다. 모든 국민은 거주·이전의 자유를 가진다. 국무총리는 대통령을 보좌하며, 행정에 관하여 대통령의 명을 받아 행정각부를 통할한다.'
+})
 
 app.set('view engine', 'ejs');
 
@@ -19,9 +38,13 @@ app.use(express.static("public"));
 
 
 app.get('/', (req,res) => {
-  res.render('home', {
-    startingContent: homeStartingContent,
-    posts: posts
+  Post.find((err, result) => {
+    if (result.length === 0){
+      samplePost.save();
+      res.redirect('/');
+    } else {
+      res.render('/', {post: result})
+    }
   })
 })
 
@@ -37,20 +60,10 @@ app.get('/compose', (req, res) => {
   res.render('compose')
 })
 
-app.post('/compose', (req, res) => {
-  const postContents = {
-    title: req.body.postTitle,
-    content: req.body.postBody,
-  };
-
-  posts.push(postContents);
-  res.redirect('/');
-})
-
 app.get('/posts/:postName', (req, res) => {
   const requestedTitle = _.lowerCase(req.params.postName);
 
-  posts.forEach(post => {
+  post.forEach(post => {
     const storedTitle = _.lowerCase(post.title);
 
     if (storedTitle === requestedTitle){
@@ -59,6 +72,15 @@ app.get('/posts/:postName', (req, res) => {
         content: post.content})
     }
   })
+})
+
+
+app.post('/compose', (req, res) => {
+  const post = new Post({
+    title: req.body.postTitle,
+    content: req.body.postBody,
+  });
+  post.save();
 })
 
 app.listen(3000, function() {
